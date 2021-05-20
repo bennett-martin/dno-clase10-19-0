12f443ea8dadd181673cfb947c1537986d41167c
bennett-martin <80833973+bennett-martin@users.noreply.github.com>
2021-05-20 00:58:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function primeraRevista() {
+    document.getElementsByTagName("body")[0].style.background = "#4c6b73";
+}
+function segundaRevista() {
+    document.getElementsByTagName("body")[0].style.background = "#ede27b";
+}
+function terceraRevista() {
+    document.getElementsByTagName("body")[0].style.background = "#eb6c05";
+}
+function cuartaRevista() {
+    document.getElementsByTagName("body")[0].style.background = "#caf272";
+}
+function nada() {
+    document.getElementsByTagName("body")[0].style.background = "#222";
+}
